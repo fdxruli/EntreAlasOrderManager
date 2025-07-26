28a86757ee282f92731ba24cc62d2d936bf8d65e
fdxruli <48109415+fdxruli@users.noreply.github.com>
2025-07-25 19:11:59 -0600

    Update sw.js
    verticalidad en telefonos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'entrealas-app-v2.2.3'; // Versión de la PWA
+const CACHE_NAME = 'entrealas-app-v2.2.4'; // Versión de la PWA
 const IMAGE_CACHE = 'entrealas-images-v2.1';
 const APP_PREFIX = self.location.host.includes('localhost') ? '' : '/EntreAlasOrderManager';
 const OFFLINE_FALLBACK = `${APP_PREFIX}/index.html`;
